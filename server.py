46b3bbb0834ad6add48a40bd41cf3cfe79208572
valefaraz <valentin@aspire.lan>
2021-12-18 15:04:48 -0300

    server

--- server.py
+++ server.py
@@ -1,17 +1,38 @@
 import socketserver
 import os
-
-class Handler(socketserver.BaseRequestHandler):
-    def handle(self):
-        print(self.client_address)
-        while True:
-            dato = self.request.recv(1024)
-            print(dato.decode())
-            self.request.sendall(b"hola")
+import asyncio
+import argparse
+import sys
 
 
-if __name__ == "__main__":
+def handle(reader, writer):
+    fd = os.open('/home/valentin/Escritorio/compu2/final/index.html', os.O_RDONLY)
+    body = os.read(fd,os.stat('/home/valentin/Escritorio/compu2/final/index.html').st_size)
+    os.close(fd)
+    dic = {"ico": "image/vnd.svf", "txt": " text/plain", "jpg": " image/jpeg",
+            "ppm": " image/x-portable-pixmap", "html": " text/html",
+            "pdf": " application/pdf"}
+    respuesta= '200 OK'
+    header = bytearray("HTTP/1.1 " + respuesta + "\r\nContent-type:" + 'html' 
+                    +"\r\nContent-length:" + str(len(body)) + "\r\n\r\n",'utf8')       
+    writer.write(header)
+    writer.write(body)
+    
 
-    socketserver.TCPServer.allow_reuse_address = True
-    server = socketserver.ThreadingTCPServer(("0.0.0.0", 8080), Handler)
-    server.serve_forever()
+
+async def main():
+    server = await asyncio.start_server(
+                    handle,
+                    ['127.0.0.1', '::1'], 
+                    8080)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+
+#if __name__ == "__main__":
+
+asyncio.run(main())
