24c3169a56e1d30117446feddde0700304916300
valefaraz <valentin@aspire.lan>
2021-12-20 14:53:35 -0300

    server

--- cliente.py
+++ cliente.py
@@ -7,9 +7,10 @@ socket.connect(("localhost", 8080))
 print("Conectado al servidor")
  
 #Creamos un bucle para retener la conexion
-while True:
+#while True:
     #Instanciamos una entrada de datos para que el cliente pueda enviar mensajes
     #mens = input("Mensaje desde Cliente a Servidor >> ")
-    time.sleep(5)
+
+#    time.sleep(5)
     #Con el método send, enviamos el mensaje
-    socket.sendall(b"10")
+socket.sendall(b"10")
--- server.py
+++ server.py
@@ -6,14 +6,19 @@ import sys
 
 
 async def handle(reader, writer):
-    fd = os.open('/home/valentin/Escritorio/compu2/final/index.html', os.O_RDONLY)
-    body = os.read(fd,os.stat('/home/valentin/Escritorio/compu2/final/index.html').st_size)
+
+    path = os.getcwd() + '/index.html'
+
+    fd = os.open(path, os.O_RDONLY)
+    body = os.read(fd,os.stat(path).st_size)
     os.close(fd)
-    dic = {"ico": "image/vnd.svf", "txt": " text/plain", "jpg": " image/jpeg",
+    
+    '''dic = {"ico": "image/vnd.svf", "txt": " text/plain", "jpg": " image/jpeg",
             "ppm": " image/x-portable-pixmap", "html": " text/html",
-            "pdf": " application/pdf"}
+            "pdf": " application/pdf"}'''
+
     respuesta= '200 OK'
-    header = bytearray("HTTP/1.1 " + respuesta + "\r\nContent-type:" + 'html' 
+    header = bytearray("HTTP/1.1 " + respuesta + "\r\nContent-type:" + 'text/html' 
                     +"\r\nContent-length:" + str(len(body)) + "\r\n\r\n",'utf8')
 
     data = await reader.read(1000)
